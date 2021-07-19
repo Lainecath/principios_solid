5a3b84bccd5569be29c54b4120314fa4858e346e
Lainecath <87619392+Lainecath@users.noreply.github.com>
2021-07-18 20:49:36 -0500

    Add files via upload

---
+++ Principio_S.py
@@ -0,0 +1,12 @@
+class coche():
+
+    def __init__(self, marca):
+        self.marca = marca
+
+    def __str__(self):
+        return "marca {}".format( self.marca )
+
+
+class CocheDB():
+
+return 
